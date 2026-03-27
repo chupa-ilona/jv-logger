8152ab6ec8c9b01ea5589b61059d3403df84a1a9
IlonaChupa <ilonachupa@gmail.com>
2026-03-27 11:06:07 +0200

    done

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,16 +2,17 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static org.apache.logging.log4j.LogManager.getLogger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
-    Logger logger = getLogger(AuthenticationServiceImpl.class);
+    private Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
-        logger.info("Method login was called");
+        logger.info("Method login was called with login: " + login + "");
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
@@ -24,5 +25,13 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
+    }
+
+    public void setLogger(Logger logger) {
+        this.logger = logger;
+    }
+
+    public Logger getLogger() {
+        return logger;
     }
 }
